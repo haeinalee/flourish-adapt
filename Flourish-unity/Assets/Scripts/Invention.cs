5232050011bda3be6e1aa9a58731814270e08a56
sliptrix <37605842+sliptrix@users.noreply.github.com>
2018-03-29 00:34:34 -0700

    Invention Object
    Adds a basic template for an invention object that stores title, id, object image, and changes it can make to the stats.

---
+++ Flourish-unity/Assets/Scripts/Invention.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Invention : MonoBehaviour {
+
+    public string title;
+    public int id;
+    public Image image;
+    public double researchTime;
+    public int changeInAirQuality = 0;
+    public int changeInWaterQuality = 0;
+    public int changeInSoilQuality = 0;
+
+    public Invention(string title, int id, Image image)
+    {
+        this.title = title;
+        this.id = id;
+        this.image = image;
+    }
+}
